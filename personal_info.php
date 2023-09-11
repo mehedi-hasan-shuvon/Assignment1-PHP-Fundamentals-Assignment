4ae9132ab03d68dc288e3f6ffbba3f909dee49e8
Mehedi Hasan Shuvon <mehedi.hasan.shuvon@gmail.com>
2023-09-11 23:50:30 +0600

    task 1 complete

--- personal_info.php
+++ personal_info.php
@@ -1,4 +1,14 @@
 <?php
-echo "Hello World";
+$name = "Mehedi Hasan Shuvon";
+
+$age = 26;
+
+
+$country = "Bangladesh";
+
+$biography="I graduated from BRAC University in Computer Science and Engineering in 2022. Then i worked as a Junior Software Engineer at Manush Technologies Ltd for one year in MERN Stack. Currently, I am working as a Software Engineer at NEXTGEN INNOVATION LTD. My goal is to deliver high-quality, industry standard software";
+
+
+echo "Hi! My name is {$name}.I am {$age} years old. I am from {$country}. \n{$biography}.";
 
 ?>
