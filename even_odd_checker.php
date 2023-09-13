93fbd4ddf31af231b73a4f853c056213bd7c8e26
Mehedi Hasan Shuvon <mehedi.hasan.shuvon@gmail.com>
2023-09-14 00:18:42 +0600

    task 4 completed

--- even_odd_checker.php
+++ even_odd_checker.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Grade Calculator</title>
+    <title>Even or Odd Checker</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <style>
         @media only screen and (max-width: 720px){
@@ -39,72 +39,34 @@
     <div class="w-50 mt-5 mx-auto container-class">
         <div class="text-center border border-1 rounded border-black p-3">
             <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; ">
-                <h1 style="border-bottom: 1px solid black">Grade Calculator</h1>
-				<div style="width: 400px" class="alert alert-danger" role="alert">
-					*Failing in one of the tests(>60) will result in a grade of F
-				</div>
-                <form method="POST" action="" id="gradeForm">
+                <h1 style="border-bottom: 1px solid black">Even or Odd Checker</h1>
+                <form method="POST" action="" id="converterForm">
                     <div class="mb-3">
-                        <input style="width: 400px" type="number" name="score1" class="form-control" id="exampleFormControlInput1" placeholder="Enter 1st test score" min=0 max=100
-							required >
+                    
+                        <input style="width: 400px" type="number" name="num" class="form-control" id="num" placeholder="Enter Number"
+							required>
                     </div>
 
-					<div class="mb-3">
-                    
-						<input style="width: 400px" type="number" name="score2" class="form-control" id="score2" placeholder="Enter 2nd test score" min=0 max=100
-						required >
-					</div>
-
-					<div class="mb-3">
-                    
-						<input style="width: 400px" type="number" name="score3" class="form-control" id="score3" placeholder="Enter 3rd test score" min=0 max=100
-						required >
-					</div>
-
-    
                     <button style="width: 400px" type="submit" class="btn btn-outline-primary" name="submit" id="submit">Submit</button>
                 </form>
 
        
                 <div  style="width: 400px" class=" output-class alert alert-info mt-3" role="alert">
 						<?php
-							$score1=0;
-							$score2=0;
-							$score3=0;
+							$number =0;
 							if(isset($_POST['submit']) && $_SERVER['REQUEST_METHOD'] == 'POST'){
 						
-
-								if(isset($_POST['score1']) && isset($_POST['score2']) && isset($_POST['score3'])){
-									$score1 = $_POST ['score1'];
-									$score2 = $_POST ['score2'];
-									$score3 = $_POST ['score3'];
-									if($score1<50 || $score2<50 || $score3<50){
-										echo "Grade: F (Failed in at least 1 test.)";
-										exit;
-									}
-
+								// check if temperature is set
+								if(isset($_POST['num'])){
+									$number = $_POST['num'];
 								}
 
-								$avg = ($score1 + $score2 + $score3) / 3;
-								$avg=number_format($avg, 2);
-								$grade="F";
-
-								if ($avg >= 90 && $avg <= 100) {
-										$grade = "A";
-								} elseif ($avg >= 80 && $avg < 90) {
-										$grade = "B";
-								}else if ($avg >= 70 && $avg < 80) {
-										$grade = "C";
-								}else if ($avg >= 60 && $avg < 70) {
-										$grade = "D";
-								}else if ($avg >= 50 && $avg < 60) {
-										$grade = "E";
+								if($number % 2 == 0){
+									echo "{$number} is an even number";
 								}else{
-									$grade = "F";
+									echo "{$number} is an odd number";
 								}
 
-								echo "Grade: {$grade} (Average: {$avg})";
-	
 							}
 								
 						?>
